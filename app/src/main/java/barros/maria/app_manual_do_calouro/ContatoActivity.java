ba723625aa627198f75556100de2bc8beb13ee1c
Rafael <rafaelbleaob@gmail.com>
2022-11-11 09:56:10 -0300

    Update contato

---
+++ app/src/main/java/barros/maria/app_manual_do_calouro/ContatoActivity.java
@@ -0,0 +1,14 @@
+package barros.maria.app_manual_do_calouro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContatoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contato);
+    }
+}
