a3905775612117c8dd03f8053ec9e0815ef7e4b4
Maria Eduarda de S. Barros <maria123duarda@gmail.com>
2022-11-18 10:00:32 -0300

    Update horario e fale conosco

---
+++ app/src/main/java/barros/maria/app_manual_do_calouro/FaleConoscoActivity.java
@@ -0,0 +1,14 @@
+package barros.maria.app_manual_do_calouro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FaleConoscoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fale_conosco);
+    }
+}
