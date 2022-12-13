167701d0282f282106301f4c87bcf68d74c4c6a2
Rafael <rafaelbleaob@gmail.com>
2022-12-12 22:35:52 -0300

    Update Horario

--- app/src/main/java/barros/maria/app_manual_do_calouro/activity/HorarioActivity.java
+++ app/src/main/java/barros/maria/app_manual_do_calouro/activity/HorarioActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -31,37 +32,43 @@ public class HorarioActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-//        Button btnPesquisar = findViewById(R.id.btnPesquisar);
-//        btnPesquisar.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                List<Horario> horariosList = new ArrayList<>();
-//
-//                Integer curso = 0;
-//
-//                Spinner spinnerCurso = findViewById(R.id.spinnerCurso);
-//                String sCurso = spinnerCurso.getSelectedItem().toString();
-//
-//                if (sCurso.equals("INFO")) {
-//                    curso = 1;
-//                } else if (sCurso.equals("MEC")) {
-//                    curso = 2;
-//                } else if (sCurso.equals("IOT")) {
-//                    curso = 3;
-//                }
-//
-//                Spinner spinnerModulo = findViewById(R.id.spinnerModulo);
-//                String sModulo = spinnerModulo.getSelectedItem().toString();
-//
-//                Integer modulo = Integer.valueOf(sModulo);
-//
-//                APIManager apiManager = new APIManager(getApplication());
-//                horariosList = apiManager.getHorario(curso, modulo);
-//
-//                TableLayout tlHorario = findViewById(R.id.tlHorario);
-//                tlHorario.addView();
-//            }
-//        });
+        Button btnPesquisar = findViewById(R.id.btnPesquisar);
+        btnPesquisar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                APIManager apiManager = new APIManager(getApplication());
+                List<Horario> horariosList = new ArrayList<>();
+
+                Integer curso = 0;
+
+                Spinner spinnerCurso = findViewById(R.id.spinnerCurso);
+                String sCurso = spinnerCurso.getSelectedItem().toString();
+
+                if (sCurso.equals("INFO")) {
+                    curso = 1;
+                } else if (sCurso.equals("MEC")) {
+                    curso = 2;
+                } else if (sCurso.equals("IOT")) {
+                    curso = 3;
+                }
+
+                Spinner spinnerModulo = findViewById(R.id.spinnerModulo);
+                String sModulo = spinnerModulo.getSelectedItem().toString();
+
+                Integer modulo = Integer.valueOf(sModulo);
+
+                TableLayout tlHorario = findViewById(R.id.tlHorario);
+
+                horariosList = apiManager.getHorario(curso, modulo);
+
+                for (int i = 0; i < horariosList.toArray().length; i++) {
+                    TableRow tr = new TableRow(getApplication());
+
+                    tlHorario.addView(tr);
+//                    tr.addView();
+                }
+            }
+        });
 
     }
 }
